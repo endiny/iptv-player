070e4bb1cff2fd515876f9fa3b2f4452e6caf77a
endiny <vl.boboshko@gmail.com>
2026-02-16 01:01:18 -0800

    Optimize route imports with lazy-loaded chunks

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import './App.css'
-import Main from './playlist-view/Main'
-import { Route, Routes } from 'react-router'
-import { HlsPlayer } from './player/HlsPlayer'
+import { lazy, Suspense } from 'react';
+import './App.css';
+import { Route, Routes } from 'react-router';
+
+const Main = lazy(() => import('./playlist-view/Main'));
+const HlsPlayer = lazy(() =>
+  import('./player/HlsPlayer').then((module) => ({ default: module.HlsPlayer }))
+);
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Main />} />
-      <Route path="/player" element={<HlsPlayer />} />
-    </Routes>
-  )
+    <Suspense fallback={<div className="p-4">Loading…</div>}>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/player" element={<HlsPlayer />} />
+      </Routes>
+    </Suspense>
+  );
 }
 
-export default App
+export default App;
